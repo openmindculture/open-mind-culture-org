1f87bd61c5cab7ed576c0dfa16a2baeb206d617c
Ingo Steinke <ingosteinke@ingosteinke.de>
2017-09-05 16:12:27 +0200

    add google analytics tracking (again) to page footer

--- themes/dekonstrukt/footer.php
+++ themes/dekonstrukt/footer.php
@@ -7,22 +7,22 @@
  * @author     	Ruben Deyhle <ruben@sprachkonstrukt.de>
  * @url		   	http://sprachkonstrukt2.deyhle-webdesign.com
  */ ?>
-		
+
 		</div>
-		
-		<?php 
+
+		<?php
 		// creating social button menu on the left
-		wp_nav_menu( array( 'container' => 'ul',  'menu_class' => 'socialbuttons', 'fallback_cb' => 'sprachkonstrukt_socialbuttons', 'theme_location' => 'social', 'depth' => 1 ) );  
+		wp_nav_menu( array( 'container' => 'ul',  'menu_class' => 'socialbuttons', 'fallback_cb' => 'sprachkonstrukt_socialbuttons', 'theme_location' => 'social', 'depth' => 1 ) );
 		?>
 		<?php
 		// creating main sidebar on the right
 		?>
-		
+
 		<?php get_sidebar(); ?>
-		
+
 		<div id="finished" ></div>
 	</div>
-		
+
 	<footer id="footer">
         <? /*
 
@@ -32,7 +32,7 @@
         ?>
 	<?php wp_footer(); ?>
 	</footer>
-	
+
 	<!-- custom footer elements -->
 <aside class="mobileads">
 
@@ -57,11 +57,17 @@
 <p>
 <a href="https://www.aerzte-ohne-grenzen.de/spenden/jetzt-online-spenden/einmalig-spenden/index.html"><img alt="Spenden für Ärzte ohne Grenzen" src="http://www.aerzte-ohne-grenzen.de/_media/bilder/banner-haupt--300x250-fallback.jpg" id="Ärzte ohne Grenzen - Onlinebanner Wir sind da" class="size-full aligncenter" /></a></p>
 <!-- /custom ad doctors without borders -->
-
-
-
-
 </aside>
-	
+
+<!-- google analytics tracking -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-105786569-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
 </body>
 </html>
