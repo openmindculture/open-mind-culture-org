ba4566710eac0d49c5653a1f92f5c6b7d692fd68
Ingo Steinke <ingosteinke@ingosteinke.de>
2017-09-29 16:15:20 +0200

    add modified files for new theme 'suri'

---
+++ themes/suri/template-parts/site-head.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template part for displaying <head> contents
+ *
+ * @package Suri
+ * @since 0.0.8
+ */
+
+?>
+
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<?php endif; ?>
