fd1614cdd2243c43bd77e340d317c6a804950b98
Ingo Steinke <ingosteinke@ingosteinke.de>
2017-10-09 17:48:10 +0200

    adjust header size, add home link

---
+++ themes/suri/template-parts/header-text.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The template part for displaying header text
+ *
+ * Display site title and site description.
+ *
+ * @package Suri
+ * @since 0.0.6
+ */
+
+?>
+
+<div<?php suri_attr( 'title-area' )?>><a href="/"></a></div><!-- .title-area -->
