8df073efd6eddce8afa37957482673e9954720a2
Ingo Steinke <ingosteinke@gmx.de>
2020-12-08 13:32:38 +0100

    use twentytwenty-child theme on localhost and website

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-www.open-mind-culture.org
+[www.open-mind-culture.org](https://www.open-mind-culture.org)
+
+This is a small blog for the main purpose to keep up with WordPress changes,
+as I have been using WordPress from the very beginning but rarely used it professionally anymore.
+
+Changelog:
+
+- 2020 Custom styles are finally deployed as a child theme (twentytwenty-child)
+The child theme will be mounted into a local WordPress running on [localhost:8077](http://localhost:8077)
+after running `docker-compose up`.
+
+- 2020 use [twentytwenty](https://wordpress.org/themes/twentytwenty/) default theme and rewrote essential custom changes as customizer css
+
+- 2020 keep using [web font "Nothing You Could Do" by Kimberly Geswein](http://www.kimberlygeswein.com/).
+
+- before: using modified [Suri](https://wordpress.org/themes/suri/) theme
--- themes/twentytwenty-child/functions.php
+++ themes/twentytwenty-child/functions.php
@@ -1,9 +1,15 @@
 <?php
 
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+function enqueue_parent_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+
+
+// disable emojicons as we will not use comments on our blog
 add_action( 'init', 'disable_wp_emojicons' );
 function disable_wp_emojicons() {
 
-    // all actions related to emojis
     remove_action( 'admin_print_styles', 'print_emoji_styles' );
     remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
     remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
@@ -12,14 +18,5 @@ function disable_wp_emojicons() {
     remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
     remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
 
-    // filter to remove TinyMCE emojis
     add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
-}
-
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
-        array( 'parenthandle' ), 
-        wp_get_theme()->get('Version') // this only works if you have Version in the style header
-    );
 }
