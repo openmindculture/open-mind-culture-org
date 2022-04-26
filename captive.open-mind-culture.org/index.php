0d4645a202a7aa6ae2a873665e96a4fdb71271b6
Ingo Steinke <ingosteinke@gmx.de>
2022-04-26 12:00:21 +0200

    add public captive page
    as discussed on
    [https://dev.to/ingosteinke/creating-a-captive-page-to-sign-into-any-public-network-19a4](https://dev.to/ingosteinke/creating-a-captive-page-to-sign-into-any-public-network-19a4)

---
+++ captive.open-mind-culture.org/index.php
@@ -0,0 +1,10 @@
+<?php
+
+	if (!headers_sent()) {
+		header('Status: 200 OK');
+		header('Cache-Control: no-cache');
+		header('Content-Type: text/html');
+		header('Strict-Transport-Security: max-age=0;');
+	}
+?><!DOCTYPE html><html lang="en"><head><title>Success - captive.open-mind-culture.org</title><meta charset="UTF-8">
+</head><body><h1>Success</h1><a href="https://www.open-mind-culture.org">Visit open-mind-culture.org</a></body></html>
